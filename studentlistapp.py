0c595cd46fafb66c8c4e02bc3e7b1330b55cef94
mrz1360100 <64081284+mrz1360100@users.noreply.github.com>
2020-04-22 09:40:57 +0200

    Update studentlistapp.py

--- studentlistapp.py
+++ studentlistapp.py
@@ -1,4 +1,4 @@
-studentlist = ["johson", "xxyy0011", "joso1122"]
+studentlist = ["johson", "xxyy0011", "joso1122", "haal1797"]
 
 print("These students have finished the lab: ")
 for student in studentlist:
